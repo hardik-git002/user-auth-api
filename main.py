e9af3ca6dfe5353d0bb2319969f1e875a9956b0c
Hardik Sheoran <hardiksheoran002@gmail.com>
2026-03-11 23:26:01 +0530

    Update CORS

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = FastAPI(title="User Auth API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
